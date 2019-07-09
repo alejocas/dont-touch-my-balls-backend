fbe823fc7c6256a9f86cfcd8a94458e41fed6e7a
Alejo Castaño <alejocas98@gmail.com>
2019-07-09 00:47:03 -0500

    Refactor due to unnecesary amount of code

--- controller/controllerOrchestrator.js
+++ controller/controllerOrchestrator.js
@@ -5,22 +5,30 @@ const { captureAttempt } = require('./gameController');
 const { CAPTURE_RESULT, LOG_INFO, PLAYER_ADDITION, PLAYER_LIST, PLAYER_UPDATE_SCORE } = BROADCAST_EVENTS;
 const { CAPTURE_ATTEMPT, PLAYER_CONNECTION } = RECEPTION_EVENTS;
 
+function emitEventToAll(socket, event, payload) {
+    socket.emit(event, payload);
+    socket.broadcast.emit(event, payload);
+}
+
 function orchest(socket) {
     console.log(socket.id);
     socket.on(PLAYER_CONNECTION, (data) => {
-        socket.emit(PLAYER_ADDITION, addPlayer(data));
-        const playersInGame = getPlayerList();
-        socket.emit(PLAYER_LIST, playersInGame);
-        socket.broadcast.emit(PLAYER_LIST, playersInGame);
+        const addPlayerResponse = addPlayer(data)
+        socket.emit(PLAYER_ADDITION, addPlayerResponse);
+        if (addPlayerResponse.connected) {
+            const playersInGame = getPlayerList();
+            const logInfoAboutPlayer = {
+                message: `Usuario ${data.name} ha ingresado a la sala`
+            }
+            emitEventToAll(socket, PLAYER_LIST, playersInGame);
+            emitEventToAll(socket, LOG_INFO, logInfoAboutPlayer);
+        }
     });
     socket.on(CAPTURE_ATTEMPT, (data) => {
         const answer = captureAttempt(data);
-        socket.emit(CAPTURE_RESULT, answer.CAPTURE_RESULT);
-        socket.emit(LOG_INFO, answer.LOG_INFO);
-        socket.emit(PLAYER_UPDATE_SCORE, answer.PLAYER_UPDATE_SCORE);
-        socket.broadcast.emit(CAPTURE_RESULT, answer.CAPTURE_RESULT);
-        socket.broadcast.emit(LOG_INFO, answer.LOG_INFO);
-        socket.broadcast.emit(PLAYER_UPDATE_SCORE, answer.PLAYER_UPDATE_SCORE);
+        emitEventToAll(socket, CAPTURE_RESULT, answer.CAPTURE_RESULT);
+        emitEventToAll(socket, LOG_INFO, answer.LOG_INFO);
+        emitEventToAll(socket, PLAYER_UPDATE_SCORE, answer.PLAYER_UPDATE_SCORE);
     });
 }
 
