4cceb594858236b3266ecb331a4619491e943e20
Alejo Castaño <alejocas98@gmail.com>
2019-07-23 23:00:48 -0500

    Changed to use an environment defined PORT

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const { express, ioLib } = require('./libraries');
 const { PORT } = require('./constants');
 const { orchest } = require('./controller/controllerOrchestrator');
 const app = express();
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const portInUse = process.env.PORT ? process.env.PORT : PORT; 
+const server = app.listen(portInUse, () => console.log(`Server running on port ${PORT}`));
 const io = ioLib(server);
 
 io.on('connection', orchest);
